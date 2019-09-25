7a0ce330a5e7a53c813207d99c647b97c2ee3489
Merijn <99054535@mydavinci.nl>
2019-09-25 11:32:35 +0200

    Game update 6
    game update

--- Game.js
+++ Game.js
@@ -1,3 +1,7 @@
 //SCP game :3
+
+
 alert("Guard: Hey you there D-class! Are you alright what happened to you? If you don't get up now I will get you terminated!");
-alert("Announcer: Attention all personal! The site is experiencing several Keter and Euclid level containment breaches full site lockdown innitiated!");
+alert("Guard on speakers: Attention all personal! The site is experiencing several Keter and Euclid level containment breaches full site lockdown innitiated!");
+alert("Guard: Oh no. We need to get to the safe zone, now!");
+alert("*Guard gets grabbed by 106* As you see the guard get grabbed by 106 and dissapear in the ground! You run away for a total of 5 hallways and you run out of breath");
