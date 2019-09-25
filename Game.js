6e326e90acde56634e732dd5122879240a82d320
Merijn <99054535@mydavinci.nl>
2019-09-25 10:13:47 +0200

    Game update 5
    game update

--- Game.js
+++ Game.js
@@ -1,1 +1,3 @@
 //SCP game :3
+alert("Guard: Hey you there D-class! Are you alright what happened to you? If you don't get up now I will get you terminated!");
+alert("Announcer: Attention all personal! The site is experiencing several Keter and Euclid level containment breaches full site lockdown innitiated!");
